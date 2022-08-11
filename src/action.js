9444c832f2bfd0339bc1127a3b05b45453bfd145
BaukaErenbay <mavlyd.saidov@gmail.com>
2022-08-11 17:26:05 +0600

    accuracy increase

--- src/action.js
+++ src/action.js
@@ -1,45 +1,53 @@
 const {BOT_TOKEN, BAR_LENGTH, CHANNEL_ID, DEBUG_CHAT_ID} = process.env
 
 const {Telegram} = require("telegraf")
-const {getDaysInYear, getDayOfYear} = require("date-fns")
+const {
+	startOfYear: getStartOfYear,
+	endOfYear: getEndOfYear,
+	differenceInMilliseconds: getDifferenceInMilliseconds,
+} = require("date-fns")
 const {saveLastPercent, getLastPercent} = require("./db.js")
 
 const telegram = new Telegram(BOT_TOKEN)
 
 module.exports = async () => {
+	const now = new Date()
+	const startOfYear = getStartOfYear(now)
+	const endOfYear = getEndOfYear(now)
+	const timePastStartOfYear = getDifferenceInMilliseconds(now, startOfYear)
+	const thisYearDuration = getDifferenceInMilliseconds(endOfYear, startOfYear)
+	const percent = Math.round((timePastStartOfYear / thisYearDuration) * 1000) / 1000
+	const percentNatural = Math.floor(percent * 100)
+	const filledPartLength = Math.floor(BAR_LENGTH * percent)
+
 	try {
 		const lastPercent = await getLastPercent()
 
-		const now = new Date()
-		const daysInYear = getDaysInYear(now)
-		const dayOfYear = getDayOfYear(now)
-
-		const percent = dayOfYear / daysInYear //count percent 0-1
-		const percentFloor = Math.floor(percent * 100) //round down 0-100
-		const filledPartLength = Math.floor(BAR_LENGTH * percent) //symbol repeat
-
-		if (percentFloor !== lastPercent) {
+		if (percentNatural !== lastPercent) {
 			const message =
 				"▓".repeat(filledPartLength) +
 				"░".repeat(BAR_LENGTH - filledPartLength) +
 				" " +
-				percentFloor +
+				percentNatural +
 				"%"
 
 			await telegram.sendMessage(CHANNEL_ID, message)
-			await saveLastPercent(percentFloor)
+			await saveLastPercent(percentNatural)
 		}
 	} catch (err) {
+		console.log(err)
 		DEBUG_CHAT_ID &&
 			(await telegram.sendMessage(
 				DEBUG_CHAT_ID,
 				`${err.toString()}\n${JSON.stringify(
 					{
-						lastPercent,
-						daysInYear,
-						dayOfYear,
+						startOfYear,
+						endOfYear,
+						timePastStartOfYear,
+						thisYearDuration,
+						percentActual: percent,
 						percent,
-						percentFloor,
+						percentNatural,
 						filledPartLength,
 					},
 					null,
